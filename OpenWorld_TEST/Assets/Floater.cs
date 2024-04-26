e12061abac08649d012474aa1d609e182aed4a67
ShadowDragon34 <okkimboy@gmail.com>
2024-04-26 14:23:24 +0300

    USUSUsusususususussuUZUZuzuzuzUUZUZ

--- OpenWorld_TEST/Assets/Floater.cs
+++ OpenWorld_TEST/Assets/Floater.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.AQUAS-Lite;
+using UnityEngine.Rendering.AQUAS-; Lite;
 
 public class Floater : MonoBehaviour
 {
